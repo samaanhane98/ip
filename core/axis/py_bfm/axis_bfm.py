66b5f51e95392ac16dbb72395bc1a99a9c138a40
Sam Aanhane <samaanhane@gmail.com>
2026-09-13 11:53:10 +0200

    Implemented BFM for AXIS source and sink

--- core/axis/py_bfm/axis_bfm.py
+++ core/axis/py_bfm/axis_bfm.py
@@ -1,18 +1,22 @@
 from dataclasses import dataclass
+from typing import Self
+
 
 import cocotb
 from cocotb.triggers import RisingEdge
+from cocotb.queue import Queue
+
 
 @dataclass
 class AxisBeat:
-    tvalid : int = 0;
-    tlast  : int = 0;
-    tdata  : int = 0;
-    tuser  : int = 0;
-    tkeep  : int = 0;
+    tvalid : int = 0
+    tlast  : int = 0
+    tdata  : int = 0
+    tuser  : int = 0
+    tkeep  : int = 0
 
     @staticmethod
-    def into_beats(data: bytearray, bus_width: int) -> list:
+    def into_beats(data: bytearray, bus_width: int) -> list[Self]:
         chunk_size = bus_width // 8
         chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
 
@@ -20,17 +24,36 @@ class AxisBeat:
         full_keep = (1 << chunk_size) - 1
         last_keep = full_keep if remainder == 0 else (1 << remainder) - 1
 
-        beats = list(
-            map(
-                lambda chunk: AxisBeat(tvalid=1, tlast=0, tdata=chunk, tuser=0, tkeep=full_keep), 
-                chunks
-            )
-        )
+        beats = [AxisBeat(tvalid=1, tlast=0, tdata=chunk, tuser=0, tkeep=full_keep) for chunk in chunks]
 
         beats[-1].tlast = 1
         beats[-1].tkeep = last_keep
 
         return beats
+
+    @staticmethod
+    def to_bytes(beats: list[Self]) -> bytearray:
+        result = bytearray()
+
+        for beat in beats:
+            tdata = beat.tdata
+            tkeep = beat.tkeep
+
+            bytes_list = [tdata[i : i - 7] for i in range(tdata.range.left, tdata.range.right, -8)]
+            bytes_list = list(reversed(bytes_list))
+
+
+            keep_bits = [bit for bit in tkeep]
+            keep_bits = list(reversed(keep_bits))
+
+            bytes_list = [b for b, k in zip(bytes_list, keep_bits) if int(k) == 1]
+
+            for byte in bytes_list:
+                result += bytes([byte])
+
+        return result
+        
+
 
 class AxisSourceBfm:
     def __init__(self, dut: object, name: str, clock: object):
@@ -38,9 +61,14 @@ class AxisSourceBfm:
         self.name = name
         self.clock = clock
 
+        self._queue = Queue[AxisBeat]()
+        self._coro = None
+
         self._signals = {}
         self._map_signals()
         self._bus_width = len(self._signals['tdata'].range)
+
+        self.start()
 
     def _map_signals(self):
         bus = self.dut._get(self.name)
@@ -53,13 +81,28 @@ class AxisSourceBfm:
             raise ValueError(f"ready not found for {self.name}")
 
         self._signals['ready'] = self.dut._get(ready_name)
-
         
         self._signals['tvalid'] = bus.tvalid
         self._signals['tlast'] = bus.tlast
         self._signals['tdata'] = bus.tdata
         self._signals['tuser'] = bus.tuser
         self._signals['tkeep'] = bus.tkeep
+
+    def start(self) -> None:
+        if self._coro is not None:
+            raise RuntimeError("Source already started")
+        self._coro = cocotb.start_soon(self._run())
+
+    def stop(self) -> None:
+        if self._coro is None:
+            raise RuntimeError("Sink never started")
+        self._coro.cancel()
+        self._coro = None
+
+    async def _run(self) -> None:
+        while True:
+            transfer = await self._queue.get()
+            await self.send_transfer(transfer)
 
     async def send_transfer(self, beat: AxisBeat):
         self._signals['tdata'].value = int.from_bytes(beat.tdata, byteorder='little')
@@ -78,4 +121,85 @@ class AxisSourceBfm:
         beats = AxisBeat.into_beats(data, self._bus_width)
         
         for beat in beats:
-            await self.send_transfer(beat)
+            await self._queue.put(beat)
+
+class AxisSinkBfm:
+    def __init__(self, dut: object, name: str, clock: object):
+        self.dut = dut
+        self.name = name
+        self.clock = clock
+
+        self._queue = Queue[AxisBeat]()
+        self._coro = None
+
+        self._signals = {}
+        self._map_signals()
+        self._bus_width = len(self._signals['tdata'].range)
+
+        self.start()
+
+    def _map_signals(self):
+        bus = self.dut._get(self.name)
+        if bus == None:
+            raise ValueError(f"Axis bus {self.name} not found")
+
+        ready_name = self.name + "_ready"
+        
+        if self.dut._get(ready_name) == None:
+            raise ValueError(f"ready not found for {self.name}")
+
+        self._signals['ready'] = self.dut._get(ready_name)
+        
+        self._signals['tvalid'] = bus.tvalid
+        self._signals['tlast'] = bus.tlast
+        self._signals['tdata'] = bus.tdata
+        self._signals['tuser'] = bus.tuser
+        self._signals['tkeep'] = bus.tkeep
+
+    def start(self) -> None:
+        if self._coro is not None:
+            raise RuntimeError("Sink already started")
+        self._coro = cocotb.start_soon(self._run())
+
+    def stop(self) -> None:
+        if self._coro is None:
+            raise RuntimeError("Sink never started")
+        self._coro.cancel()
+        self._coro = None
+
+    async def _run(self) -> None:
+        while True:
+            transfer = await self.receive_transfer()
+            await self._queue.put(transfer)
+
+    async def receive_transfer(self) -> AxisBeat:
+        self._signals['ready'].value = 1
+
+        await RisingEdge(self.clock)
+        while self._signals['tvalid'].value != 1:
+            await RisingEdge(self.clock)
+
+        self._signals['ready'].value = 0
+
+        return AxisBeat(
+            tvalid=self._signals['tvalid'].value,
+            tlast=self._signals['tlast'].value,
+            tdata=self._signals['tdata'].value,
+            tuser=self._signals['tuser'].value,
+            tkeep=self._signals['tkeep'].value,
+        )
+
+    async def receive(self) -> bytearray:
+        beats = []
+
+        while True:
+            beat = await self._queue.get()
+            beats.append(beat)
+
+            if beat.tlast == 1:
+                break
+
+        data = AxisBeat.to_bytes(beats)
+        return data
+
+        
--- core/axis/py_tc/axis_fifo_sync_tc.py
+++ core/axis/py_tc/axis_fifo_sync_tc.py
@@ -1,10 +1,13 @@
+import random
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer, ClockCycles, ReadOnly 
+from cocotb.handle import Freeze, Release
 
 from test_harness import TestHarness, test
 
-from axis_bfm import AxisSourceBfm
+from axis_bfm import AxisSourceBfm, AxisSinkBfm, AxisBeat
 
 
 class TestCase(TestHarness):
@@ -15,6 +18,7 @@ class TestCase(TestHarness):
         cocotb.start_soon(Clock(self.dut.clk, 10, unit="ns").start())
 
         self.source = AxisSourceBfm(self.dut.i_dut, "stream_in", self.dut.clk)
+        self.sink = AxisSinkBfm(self.dut.i_dut, "stream_out", self.dut.clk)
 
     async def reset_tc(self, cycles = 10):
         self.dut.reset.value = 1
@@ -25,8 +29,39 @@ class TestCase(TestHarness):
     
     @test
     async def test_001_write_test(self):
-        data = bytes([x for x in range(50)])
-        await self.source.send(data)
+        # Explicitly disable sink so we can monitor the write level
+        self.sink.stop()
+        self.dut.stream_out_ready.value = 0
+
+        amount = 0xf
+        for index in range(amount):
+            data = bytes([index])
+            beat = AxisBeat(1, 1, data, 0, 1)
+            await self.source.send_transfer(beat)
+
+        await ClockCycles(self.dut.clk, 1)
+
+        write_level = self.dut.wr_level.value.to_unsigned()
+        assert write_level == amount
+
+        self.sink.start()
+
+    @test
+    async def test_002_read_test(self):
+        # TODO: FIFO is not empty after reset
+        await self.reset_tc()
+
+        amount = 0x10
+
+        for i in range(amount):
+            data_length = random.randint(1, 256)
+            data = random.randbytes(data_length)
+
+            await self.source.send(data)
+            recv = await self.sink.receive()
+
+            assert data.hex() == recv.hex()
+
 
 
 @cocotb.test()
